4fd80e2b5fe0ae51c51c85e6e4146d29c7b245a9
romcanek <71186827+romcanek@users.noreply.github.com>
2022-01-25 21:56:03 +0100

    Delete scripts directory

--- scripts/highscores.js
+++
@@ -1,8 +0,0 @@
-const high = document.getElementById("highscore_text")
-const clear = document.getElementById("clear")
-high.innerText = `Tvoje nejvyšší skóre je ${localStorage.getItem("highscore")}, dáš lepší?`
-
-clear.addEventListener("click", () => {
-    localStorage.setItem("highscore", 0)
-    high.innerText = `Tvoje nejvyšší skóre je ${localStorage.getItem("highscore")}, dáš lepší?`
-})
